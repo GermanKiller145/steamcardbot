520775b9a3d30a4d72727ed6425a3bfafc0a7219
GermanKiller145 <germankiller145@gmail.com>
2018-03-29 23:31:04 +0200

    Added debug, prices, dynamic game

--- index.js
+++ index.js
@@ -13,18 +13,23 @@ client.on('loggedOn', () => {
   console.log('Logged into Steam!');
 
   client.setPersona(SteamUser.Steam.EPersonaState.Online);
-
-  client.gamesPlayed("Selling Card Sets | 1 CSGO Key = 17 Sets")
+  if(config.debug === true){
+    client.gamesPlayed(config.debgame)
+  } else {
+    client.gamesPlayed("1 CSGO Key - " + config.sets_csgokey + " Sets | 1 TF2 - " + config.sets_tfkey + "Sets");
+  }
+  
 });
 
 client.on('friendMessage', function(steamID, message) {
   params = message.split(" ", 2);
   if(params[0] === "!buy"){
     if(params[1] <= "0"){
-      client.chatMessage(steamID, "Please type a amout of keys you wanna pay.");
+      client.chatMessage(steamID, "Please type an amout of keys you wanna pay.");
     } else {
       num = params[1];
       client.chatMessage(steamID, "Processing...");
+      num_sets = num * config.sets_csgokey;
       client.chatMessage(steamID, "I've send you a trade offer with " + num_sets + " Sets.");
     }
     console.log("Client: " + steamID.getSteam3RenderedID() + "; Message: " + message);
