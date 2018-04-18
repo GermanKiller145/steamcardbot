93e58e6675a1f19d4289cdd8236f8c4eafccdaab
GermanKiller145 <germankiller145@gmail.com>
2018-04-18 21:54:39 +0200

    added friendrelationship and help cmd

--- index.js
+++ index.js
@@ -18,7 +18,16 @@ client.on('loggedOn', () => {
   } else {
     client.gamesPlayed("1 CSGO Key - " + config.sets_csgokey + " Sets | 1 TF2 - " + config.sets_tfkey + "Sets");
   }
-  
+
+});
+
+client.on('friendRelationship', (steamID, relationship) => {
+  if(relationship == EFriendRelationship.RequestRecipient){
+    client.addFriend(steamID);
+    client.chatMessage(steamID, "Hello and welcome to the SteamCardBot by GermanKiller!");
+    client.chatMessage(steamID, "For information how to use this bot you can type !help and he will send you the commands.");
+    client.chatMessage(steamID, "Have fun with leveling!");
+  }
 });
 
 client.on('friendMessage', function(steamID, message) {
@@ -32,6 +41,10 @@ client.on('friendMessage', function(steamID, message) {
       num_sets = num * config.sets_csgokey;
       client.chatMessage(steamID, "I've send you a trade offer with " + num_sets + " Sets.");
     }
+    console.log("Client: " + steamID.getSteam3RenderedID() + "; Message: " + message);
+  }
+  else if(params[0] === "!help"){
+    client.chatMessage(steamID, "");
     console.log("Client: " + steamID.getSteam3RenderedID() + "; Message: " + message);
   }
 });
